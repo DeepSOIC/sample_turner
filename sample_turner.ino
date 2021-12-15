ac7c7386dc603dd850b3a9d8e48edc4ebcb03757
DeepSOIC <vv.titov@gmail.com>
2021-12-16 02:46:57 +0300

    tidyups and comments

--- sample_turner.ino
+++ sample_turner.ino
@@ -1,4 +1,4 @@
-#define PIN_STEP 9
+#define PIN_STEP 9 //do not change! tied to OCR1A
 #define PIN_DIR 8
 #define PIN_SLP 7
 float lnfmin;
@@ -7,7 +7,8 @@ float lnfmax;
 #include <math.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);//for debug output, if needed
+
   digitalWrite(PIN_STEP, LOW);
   pinMode(PIN_STEP, OUTPUT);
   digitalWrite(PIN_DIR, LOW);
@@ -17,9 +18,8 @@ void setup() {
 
   lnfmin = log(10);
   lnfmax = log(60000);
-  Serial.begin(9600);
 
-  //set up timer1
+  //set up timer1 into mode 14 (fast pwm with top=ICR1)
   TCCR1A=0b10000010;
   TCCR1B=0b00011001;
   ICR1 = 4096;
@@ -33,7 +33,10 @@ void loop() {
   //Serial.println(f);
   float period = 1.0/f;
   float period_cyc = period*fcpu;
-  unsigned int iperiod = 65535;
+  unsigned int iperiod = 65535; //the new timer period (to be computed)
+
+  //determine the best prescaler (pick the lowest 
+  //division where period fits into 16 bits of counting)
   byte prescaler = 0;
   if(period_cyc>65535.0*256.0){
     prescaler = 0b00000101;
@@ -56,9 +59,13 @@ void loop() {
     iperiod = round(period_cyc);
   }
 
-  //wait to apply prescaler
-  while(TCNT1>4){};
+  //wait for the timer counter value to be pretty low
+  //to apply the new prescaler and top, 
+  //so that the timer doesn't miss the new top
+  while(TCNT1>4){}; //fixme: revise? 4 is pretty low...
+  
   TCCR1B=0b00011000 | prescaler;
   ICR1 = iperiod;
-  OCR1A=10;
+  
+  OCR1A=10;//sets pulse width
 }
